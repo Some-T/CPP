f75846b0774b98498ef670837a457dce14067c11
Jamie Cropley <somet@protonmail.com>
2020-12-03 16:47:43 +0000

    Create main.h

---
+++ University/FSMExample/main.h
@@ -0,0 +1,15 @@
+//
+// Created by Jamie on 23/07/2019.
+//
+
+#ifndef FSMEXAMPLE_MAIN_H
+#define FSMEXAMPLE_MAIN_H
+
+#endif //FSMEXAMPLE_MAIN_H
+
+enum testwords
+{
+    TESTWORD1,TESTWORD2,TESTWORD3
+};
+
+
